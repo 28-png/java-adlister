aef72ae7dcc279f406d9e572a09c3f3aeb356706
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-26 07:32:04 -0600

    updated

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,14 +5,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.Integer.parseInt;
+
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 
+
+
 public class HelloWorldServlet extends HttpServlet {
+    Integer currentPage;
+    String sortBy;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+            if(sortBy != null) {
+                sortBy = req.getParameter("sort");
+            }
+
+            if(currentPage != null) {
+         currentPage = parseInt(req.getParameter("page"));
+            }
+
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-         out.println("<h1>Hello, World!</h1>");
+         out.println("<h1>Hello, World!</h1>" +
+                 "Hello " + sortBy + "!" + "<br>" +
+                 "Page: " + currentPage);
     }
 }
