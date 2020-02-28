b122077b6f517e627cb448bac4fcc6f41c095d52
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-28 15:15:48 -0600

    updated

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -7,7 +7,13 @@ import java.io.IOException;
 
 @WebServlet("/login.jsp")
 public class LoginServlet extends HttpServlet {
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         if (request.getMethod().equalsIgnoreCase("post")) {
             String username = request.getParameter("username");
             String password = request.getParameter("password");
--- src/main/webapp/partials/navbar.jsp
+++ src/main/webapp/partials/navbar.jsp
@@ -4,8 +4,8 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="/ads">Adlister</a>
         </div>
-        <ul class="nav navbar-nav navbar-right">
-            <li><a href="/login">Login</a></li>
+       <ul class="nav navbar-nav navbar-right">
+           <li><a href="/login.jsp">Login</a></li>
         </ul>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
