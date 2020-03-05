1a2f14c01af1635a517c02b029951dbe43c0cc73
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-05 11:35:09 -0600

    updated

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -3,11 +3,18 @@ import com.codeup.adlister.dao.Config;
 public class DaoFactory {
     private static Ads adsDao;
     private static Config config;
-
+    private static Users usersDao;
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new MySQLAdsDao(config);
         }
         return adsDao;
+    }
+
+    public static Users getUsersDao() {
+        if(usersDao == null) {
+            usersDao = new MySQLUsersDao();
+        }
+        return usersDao;
     }
 }
