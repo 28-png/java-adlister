1a63e053dad2f7ed4b626985ec26290be78b10c8
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-03-06 06:42:04 -0600

    updated

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -1,19 +1,70 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
 
 public class MySQLUsersDao implements Users {
+    private Connection connection = null;
 
+    public MySQLUsersDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection(
+                    config.getUrl(),
+                   config.getUsername(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
 
 
 
 
     @Override
     public User findByUsername(String username) {
+        String query = "SELECT * FROM users WHERE username LIKE ?";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            return extractUser(statement.executeQuery());
+        } catch(SQLException ex) {
+            ex.getStackTrace();
+        }
         return null;
     }
 
+    @Override
     public Long insert(User user) {
+        String query = "INSERT INTO users(username, email, password) VALUES(?, ?, ?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUsername());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+       } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+
+    private User extractUser(ResultSet rs) throws SQLException {
+        if (! rs.next()) {
+            return null;
+        }
+        return new User(
+                rs.getLong("id"),
+                rs.getString("username"),
+                rs.getString("email"),
+                rs.getString("password")
+        );
     }
 }
